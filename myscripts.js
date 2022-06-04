98b03820085630084514bd2359a5cf198f34c05d
Vaibhav.8june@gmail.com <88285949+colorfulbutterflies@users.noreply.github.com>
2022-06-05 01:10:09 +0530

    Added playGround function in Javascript

--- myscripts.js
+++ myscripts.js
@@ -4,3 +4,23 @@ function computerPlay(){
     else if(x>1 && x<=2 ){return "paper";}
     else return "scissors";
 }
+const computerSelection=computerPlay();
+const playerselection="rock";
+
+function playGround(playerselection,computerSelection){
+    if (playerselection=="rock"){
+        if(computerSelection=="rock"){return "Draw";}
+        else if(computerSelection=="paper"){return "Computer wins";}
+        else if(computerSelection=="scissors"){return "Player wins";}
+    }
+    else if (playerselection=="paper"){
+        if(computerSelection=="rock"){return "Player wins";}
+        else if(computerSelection=="paper"){return "Draw";}
+        else if(computerSelection=="scissors"){return "Computer wins";}
+    }
+    else{
+        if(computerSelection=="rock"){return "Computer wins";}
+        else if(computerSelection=="paper"){return "Player wins";}
+        else if(computerSelection=="scissors"){return "Draw";}
+    }
+}
