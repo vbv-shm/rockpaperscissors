eb45ea2f6f98187ed033abbf643da6b8d07cff8f
Vaibhav.8june@gmail.com <88285949+colorfulbutterflies@users.noreply.github.com>
2022-06-05 01:43:52 +0530

    Final code till first assignment..working fine

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,14 @@
 function computerPlay(){
     let x =Math.random()*3;
-    if (x<=1){return "stone";}
-    else if(x>1 && x<=2 ){return "paper";}
-    else return "scissors";
+    if (x<=1){
+        console.log("computer selected rock");
+        return "rock";}
+    else if(x>1 && x<=2 ){
+        console.log("computer selected paper");
+        return "paper";}
+    else 
+    console.log("computer selected scissors");
+    return "scissors";
 }
 
 
@@ -21,21 +27,27 @@ function playGround(playerselection,computerSelection){
         if(computerSelection=="rock"){return "Computer wins";}
         else if(computerSelection=="paper"){return "Player wins";}
         else if(computerSelection=="scissors"){return "Draw";}
-    }
+    };
 }
 
 function game(){
     let playerScore=0;
     let computerScore=0;
     for (let i = 0; i < 5; i++) {
+        console.log("Its round " +(i+1));
         const computerSelection=computerPlay();
-        const playerselection="rock";
+        const playerselection=window.prompt();
+        console.log("Player selected "+playerselection)
         result=playGround(playerselection,computerSelection);
-        if (result=="Player wins"){playerScore=playerScore+1;}
-        if (result=="Computer wins"){computerScore=computerScore+1;}
+        if (result=="Player wins"){
+            console.log("Player won round " +(i+1));
+            playerScore=playerScore+1;}
+        if (result=="Computer wins"){
+            console.log("Computer won round " +(i+1));
+            computerScore=computerScore+1;}
         
      }
-     return "players score is $(playerScore) and computer score is $(computerScore)";
+     return "players score is "+playerScore+" and computer score is "+computerScore;
 
 }
 
